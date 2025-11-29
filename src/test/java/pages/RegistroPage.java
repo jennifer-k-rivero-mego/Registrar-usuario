b0cc24b8552ea3f283c825c945096e5ad3fd75e7
jennifer-k-rivero-mego <jenniferk.rivero.mego@gmail.com>
2025-11-29 18:33:37 -0500

    Update RegistroPage.java

--- src/test/java/pages/RegistroPage.java
+++ src/test/java/pages/RegistroPage.java
@@ -37,7 +37,7 @@ public class RegistroPage {
         driver.findElement(radioTratamientoSra).click();
         driver.findElement(txtNombre).sendKeys("Jennifer");
         driver.findElement(txtApellidos).sendKeys("RM");
-        driver.findElement(txtCorreo).sendKeys("jennifer_rm_61@gmail.com");
+        driver.findElement(txtCorreo).sendKeys("jennifer_rm_91@gmail.com");
         driver.findElement(txtContrasena).sendKeys("Jennifer_rm_01#$");
         driver.findElement(txtFechaNacimiento).sendKeys("27/07/1997");
         driver.findElement(chkTerminos).click();
